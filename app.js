e817dfa175637a7bb9bfabd0fe325787f54b9cc2
Italo Seara <54297273+italoseara@users.noreply.github.com>
2020-09-21 23:23:09 -0300

    New Rule / Final score input
    New rule -> if you get two 6 in a roll you will lose everything

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 // Variable declaration
-var scores, roundScore, activePlayer, gameState;
+var scores, roundScore, activePlayer, gameState, dice, lastDice, finalScore;
 
 // init the game
 init();
 
 // Button to roll the dice
 document.querySelector('.btn-roll').addEventListener('click', function() {
-
     if (gameState) {
-        // Random number (1 to 6)
-        var dice = Math.floor(Math.random() * 6) + 1;
 
+        finalScore = document.querySelector('.input-score').value; // Select the input box
+
+        if (isNaN(parseInt(finalScore))) { // verify if it is a number in the input
+            finalScore = 100; // if it's not, so the finalScore is set to 100 as default
+        } else { 
+            finalScore = parseInt(finalScore); // if it is, so the finalScore is set as the input
+        }
+
+        dice = Math.floor(Math.random() * 6) + 1; // Random number (1 to 6)
         // Display the number
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block'; // Turn it visible again
@@ -20,13 +26,19 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         // Update the round if the rolled number is not a 1
         var currentPlayer = document.getElementById('current-' + activePlayer);
 
-        if (dice !== 1) {
-            roundScore += dice; // Add to the round score the dice value
-            currentPlayer.textContent = roundScore; // Display on the screen
+        if (dice === 1) { // Loses the current score when rolls 1
+            switchPlayers();
+
+        } else if (dice === 6 && lastDice === 6) { // Lose all the score when rolls two 6 in a roll
+            scores[activePlayer] = 0 // Reset the global score
+            document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]; // Refresh the global score
+            switchPlayers()
 
         } else {
-            currentPlayer.textContent = 0; // Reset the display score
-            switchPlayers();    
+            roundScore += dice; // Add to the round score the dice value
+            currentPlayer.textContent = roundScore; // Display on the screen
+        
+        lastDice = dice; // Last dice number
         };
     };
 });
@@ -40,7 +52,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         document.getElementById('score-' + activePlayer).textContent = scores[activePlayer]; // Display on the screen
         document.querySelector('.dice').style.display = 'none';
         
-        if (scores[activePlayer] >= 100) { // Check if someone won the game
+        if (scores[activePlayer] >= finalScore) { // Check if someone won the game
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
@@ -49,6 +61,8 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         } else {
             switchPlayers();
         }
+
+    lastDice = undefined;
     };
 });
 
@@ -64,7 +78,7 @@ function init() {
     document.getElementById('score-0').textContent = 0;
     document.getElementById('score-1').textContent = 0;
     document.getElementById('current-0').textContent = 0;
-    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
     document.getElementById('name-0').textContent = 'Player 1';
     document.getElementById('name-1').textContent = 'Player 2';
     document.querySelector('.dice').style.display = 'none';
@@ -77,6 +91,10 @@ function init() {
 };
 
 function switchPlayers() {
+    lastDice = undefined;
+
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
     // Remove the active class
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     
